a3838a1721f1dacfa9a1637fecae6a8149178d06
SatoriMode <sebasthianmella13@gmail.com>
2026-08-13 23:03:44 -0400

    Create product.js

---
+++ js/product.js
@@ -0,0 +1,11 @@
+const PRODUCTS = [
+    {
+        id: "kid-buu",
+        name: "Polera Kid Buu",
+        category: "Anime",
+        price: 18990,
+        image: "productos/anime/polera-kid-buu-01.PNG",
+        url: "productos/anime/polera-kid-buu.html",
+        tags: ["kid buu", "buu", "dragon ball", "anime"]
+    }
+];
